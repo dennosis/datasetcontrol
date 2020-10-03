f428e7a0bb8c04b4ffc4c7da6776ce5bad86ffc7
dennosis <dennisaguiar@hotmail.com>
2020-10-03 15:49:49 -0300

    dinamizado para pegar infos do backend

--- src/App.js
+++ src/App.js
@@ -1,65 +1,198 @@
-import React,{useState} from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from "react-router";
+import { useHistory } from "react-router-dom";
+
+import { Container } from 'reactstrap';
+import api from './api'
+
 import { Control } from './Control.js'
-import { models }  from './importsSVG.js'
+import { descriptionObj, saveModel, setModelSvgDOM, validatePositiveNumber } from './utils.js'
+
 
 
 function App() {
+   
+    const history = useHistory()
+    const { indexRouter } = useParams()
 
-  const [ index, setIndex ] = useState(0);
-  const [ colorStyle, setColorStyle ] = useState(true);
+    const [index, setIndex] = useState(0);
+    
+    const [models, setModels] = useState([]);
+    const [modelsError, setModelsError] = useState(false);
 
-  const { Model, path }  =  models[index]
+    const [description, setDescription] = useState({});
+    const [descriptionIsError, setDescriptionIsError] = useState(false);
+    const [descriptionIsFinding, setDescriptionIsFinding] = useState(false);
 
-  return (
-    <section className={colorStyle?'app':''}>
+    const [saving, setSaving] = useState(false);
+    const [saveAllMode, setSaveAllMode] = useState(false);
 
-      <Control 
-        index={index} 
-        setIndex={setIndex} 
-        length={models.length} 
-        path={path} 
-        colorStyle={colorStyle}
-        setColorStyle={setColorStyle}
-      />
-      
-      <section id="model" className="m-5">
-        <Model />
-      </section>
+    const [color, setColor] = useState(true);
+    const [furniture, setFurniture] = useState(false);
+    const [openings, setOpenings] = useState(false);
+    const [segmentation, setSegmentation] = useState(false);
+    const [saveApi, setSaveApi] = useState(true);
+    const [saveImg, setSaveImg] = useState(false);
+    
 
-    </section>
 
-  );
+    let style = []
+
+    if (color) style.push('color')
+    if (!furniture) style.push('furniture')
+    if (!openings) style.push('openings')
+    if (segmentation) style.push('segmentation')
+
+    const saveAllModel=()=>{
+        setSaving(false)
+        setSaveAllMode(true)
+    }
+
+    const updateUrl = (indexModel=0, maxIndexModel=1) => {
+
+        const validateIndexModel = validatePositiveNumber(indexModel)
+
+        if(validateIndexModel < maxIndexModel){
+            setIndex(validateIndexModel)
+            history.replace(`/${validateIndexModel+1}`)
+        }else{
+            setIndex(maxIndexModel-1)
+            history.replace(`/${maxIndexModel}`)
+        }
+        
+    }
+
+    useEffect(() => {
+
+        if (!models.length && !modelsError) {
+            api.getPlains().then(
+                async res => {
+                    setModels(res.data)
+                    updateUrl(indexRouter, res.data.length)
+                },
+                error => {
+                    setModelsError(true)
+                }
+            ).finally(()=>updateUrl())
+            
+
+        }
+        
+        if(models.length){
+            if (!descriptionIsError && (description._id !== models[index]._id)) {
+                if(!descriptionIsFinding){
+                    setDescriptionIsFinding(true)
+                    api.getPlainById(models[index]._id)
+                        .then(
+                            res => {
+                                setDescription(res.data)
+                                setModelSvgDOM(res.data.svg)
+                            },
+                            error => {
+                                setDescription({})
+                                setDescriptionIsError(true)
+                            }
+                        )
+                        .finally(()=>setDescriptionIsFinding(false))
+                }
+                
+            }else{
+                if(saveAllMode && !saving){
+                    if(index < models.length){
+                        setSaving(true)
+                        saveModel(descriptionObj(models[index].path),                   
+                            saveApi,
+                            saveImg,
+                            ()=>{
+                                return setTimeout(()=>{    //dispara depois de 1.0 segundos
+                                    const newIndex = index+1
+                                    if(newIndex === models.length){
+                                        setSaveAllMode(false)
+                                    } else {
+                                        setDescriptionIsError(false)
+                                        updateUrl(newIndex, models.length)
+                                    }
+                                    setSaving(false)
+                                }, 1000)
+                            }
+                        )
+                        
+                    }else{
+                        setSaveAllMode(false)
+                    }
+                }    
+            }
+        }
+
+
+    }, [
+        indexRouter,
+        index, 
+        models, 
+        modelsError,
+        description, 
+        descriptionIsError, 
+        descriptionIsFinding, 
+        saveAllMode, 
+        saving, 
+        saveApi, 
+        saveImg
+    ]);
+
+    return (
+        <Container className={style.join(' ')}>
+            <Control
+                index={index}
+                setIndex={(index)=>{updateUrl(index, models.length); setDescriptionIsError(false); }}
+                length={models.length}
+                color={color}
+                setColor={setColor}
+                furniture={furniture}
+                setFurniture={setFurniture}
+                openings={openings}
+                setOpenings={setOpenings}
+                segmentation={segmentation}
+                setSegmentation={setSegmentation}
+                saveApi = {saveApi}
+                setSaveApi = {setSaveApi}
+                saveImg = {saveImg} 
+                setSaveImg = {setSaveImg}
+
+                saveModel={() => saveModel(descriptionObj(models[index].path),saveApi,saveImg)}
+                saveAllModel={()=>saveAllModel(models.length)}
+
+            />
+            { 
+                description &&
+                <section className="p-3">
+                    <h2>name: {description.name}</h2>
+                    <h3>dimensions: {`${description.width}m x ${description.height}m`}</h3>
+                </section>
+            }
+            <div className="d-flex">
+                {
+                    description &&
+                    <section className="p-3" style={{ minWidth: '500px' }}>
+                        {
+                            description.spaces &&
+                            description.spaces.map((space, index) => (
+                                <h6 key={index} className="d-flex align-items-end">
+                                    <svg viewBox="0 0 20 20" className="mr-1" style={{ width: '20px', height: '20px' }} ><g className={`${(space.class).replaceAll('.', ' ')}`} stroke="#000000" style={{ fillOpacity: 1, strokeWidth: 2, strokeOpacity: 1 }}><rect x="0" y="0" width="100%" height="100%" /></g></svg>
+                                    {`${space.name}_${space.index}: area(${space.area}m²) max(${space.width}m x ${space.height}m) pos(${space.horizontally},${space.vertically})`}
+                                </h6>
+                            ))
+                        }
+                    </section>
+                }
+                <section id="model" className="p-5">
+                    <div id="plain">
+
+                    </div>
+                </section>
+            </div>
+        </Container>
+
+    );
 }
 
 export default App;
-
-
-
-/*
-
-const ModelSVG = ({ name, ...rest }) => {
-  const ImportedIconRef =  React.useRef();
-  const [loading, setLoading] = React.useState(false);
-  React.useEffect(() => {
-    setLoading(true);
-    const importIcon = async () => {
-      try {
-        // Changing this line works fine to me
-        ImportedIconRef.current = (await import(`!!@svgr/webpack?-svgo,+titleProp,+ref!./datasets/cubicasa5k/colorful/30/model.svg`)).default;
-      } catch (err) {
-        throw err;
-      } finally {
-        setLoading(false);
-      }
-    };
-    importIcon();
-  }, [name]);
-
-  if (!loading && ImportedIconRef.current) {
-    const { current: ImportedIcon } = ImportedIconRef;
-    return <ImportedIcon {...rest} />;
-  }
-  return null;
-};
-
-*/
--- src/Control.js
+++ src/Control.js
@@ -1,98 +1,39 @@
-import React,{useState} from 'react';
-//import htmlToImage from 'html-to-image';
-import domtoimage from 'dom-to-image';
+import React from 'react';
 
-import { saveAs } from 'file-saver';
+import { Button, Container, Row, Col, ButtonGroup} from 'reactstrap';
 
-import { Button, Container, Row, Col, Pagination, PaginationItem, PaginationLink, ButtonDropdown, DropdownToggle, DropdownMenu, DropdownItem} from 'reactstrap';
+import { GenericPagination } from './GenericPagination'
 
-
-export function Control({index, setIndex, length=1, path, colorStyle, setColorStyle}) {
-    const [dropdownOpen, setOpen] = useState(false);
-
-    const toggle = () => setOpen(!dropdownOpen);
-  
-
-    const saveModelPng=()=>{
-        domtoimage.toBlob(document.getElementById('model'))
-        .then(function (blob) {
-            saveAs(blob, 'temp.png');
-        });
-    }
-
-    const selectPage=(pageIndex)=>{
-        let selected = pageIndex
-        selected = pageIndex >= length?length-1:selected
-        selected = pageIndex < 0 ? 0 : selected
-        setIndex(selected)
-    }
-
-    const createMapPages=(currentIndex, numPages)=>{
-        let pages = []
-        const midNumPages = Math.trunc(numPages/2)
-        const fisrt = currentIndex - midNumPages > 0 ? currentIndex - midNumPages : 0
-        const last = fisrt + numPages
-        const init = last > length-1 ? length-1-numPages : fisrt
-        for(let i=init; i <= init+numPages; i++){
-            pages.push(i)
-        }
-        return pages
-    } 
-
-  return (
-    <Container className="pt-3" >
+export function Control({index, setIndex, length=1, path, id, color, setColor, furniture, setFurniture, openings, setOpenings, segmentation, setSegmentation, saveModel, saveAllModel, saveApi, setSaveApi, saveImg, setSaveImg}) {
+    return (
+        <Container className="pt-3" >
             <Row className="justify-content-md-center">
                 <Col md="auto">
-                    <Button onClick={()=>saveModelPng()} color="primary">Save</Button>
+                    <Button onClick={()=>saveAllModel()} color="secondary">Save All</Button>
                 </Col>
                 <Col md="auto">
-                    <Pagination aria-label="Page navigation example" className='mb-auto'>
-                        <PaginationItem>
-                            <PaginationLink first href="#" onClick={()=>selectPage(0)} />
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationLink previous href="#" onClick={()=>selectPage(index-1)} />
-                        </PaginationItem>
-                        { 
-                            createMapPages(index, 5).map(page =>(
-                                <PaginationItem key={page} active={page===index} >
-                                    <PaginationLink href="#" onClick={()=>selectPage(page)}>
-                                        {page+1}
-                                    </PaginationLink>
-                                </PaginationItem>
-                            ))
-                        }
-                        <PaginationItem>
-                            <PaginationLink next href="#" onClick={()=>selectPage(index+1)} />
-                        </PaginationItem>
-                        <PaginationItem>
-                            <PaginationLink last href="#" onClick={()=>selectPage(length-1)} />
-                        </PaginationItem>
-                    </Pagination>
+                    <Button onClick={()=>saveModel()} color="secondary">Save</Button>
                 </Col>
-
                 <Col md="auto">
-                    <ButtonDropdown isOpen={dropdownOpen} toggle={toggle}>
-                        <DropdownToggle caret>
-                            Tipo de exibição
-                        </DropdownToggle>
-                        <DropdownMenu>
-                            <DropdownItem onClick={()=>setColorStyle(true)} disabled={colorStyle} >Cor</DropdownItem>
-                            <DropdownItem onClick={()=>setColorStyle(false)} disabled={!colorStyle} >wireframe</DropdownItem>
-
-                        </DropdownMenu>
-                    </ButtonDropdown>
-                
+                    <ButtonGroup>
+                        <Button color={saveApi?"secondary":"outline-secondary"} onClick={()=>setSaveApi(!saveApi)}>api</Button>
+                        <Button color={saveImg?"secondary":"outline-secondary"} onClick={()=>setSaveImg(!saveImg)}>img</Button>
+                    </ButtonGroup>
                 </Col>
-
-            </Row>
-            <Row className="justify-content-md-center">
                 <Col md="auto">
-                    <h2>{path}</h2>
+                    <GenericPagination index={index} setIndex={setIndex}  length={length} />
+                </Col>
+                <Col md="auto">
+                    <ButtonGroup>
+                        <Button color={color?"secondary":"outline-secondary"} onClick={()=>setColor(!color)}>Color</Button>
+                        <Button color={furniture?"secondary":"outline-secondary"} onClick={()=>setFurniture(!furniture)}>Furniture</Button>
+                        <Button color={openings?"secondary":"outline-secondary"} onClick={()=>setOpenings(!openings)} >Openings</Button>
+                        <Button color={segmentation?"secondary":"outline-secondary"} onClick={()=>setSegmentation(!segmentation)}>Segmentation</Button>
+                    </ButtonGroup>
                 </Col>
             </Row>
-    </Container>
-  );
+        </Container>
+    );
 }
 
 export default Control;
